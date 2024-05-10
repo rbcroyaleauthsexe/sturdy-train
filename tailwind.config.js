5d7362ae4465ec19b4e3c1d36b0fe157dfe97b16
rbcroyaleauthsexe <mitronniie@gmail.com>
2024-05-10 10:34:00 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: [
+    "./src/pages/**/*.{html,js,jsx,ts,tsx}",
+    "./src/sections/**/*.{html,js,jsx,ts,tsx}",
+    "./src/components/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  plugins: [],
+}
